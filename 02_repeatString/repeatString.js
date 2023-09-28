89fa2115923663e4e6a34ca859e9b51b5a27f4db
Freddy Chen <77744025+freddycdesign@users.noreply.github.com>
2023-09-28 12:39:54 -0700

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,19 +1,18 @@
 const repeatString = function (word, times) {
-    if (times < 0) return "ERROR";
+    if (times < 0) return "ERROR"
     let string = "";
     for (let i = 0; i < times; i++) {
         string += word;
     }
     return string;
-};
-
-repeatString('hey', 3);
-repeatString('hey', 10);
-repeatString('hey', 1);
-repeatString('hey', 0);
-repeatString('hey', -1);
-repeatString('hey', Math.Random);
-repeatString('hey', undefined);
+}
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -3);
+repeatString("hey", Math.random);
+repeatString("hey", null);
 
 // Do not edit below this line
 module.exports = repeatString;
