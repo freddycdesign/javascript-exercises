db7d961193551f2083a56dce7b36681304a77a0a
Freddy Chen <77744025+freddycdesign@users.noreply.github.com>
2023-10-10 11:43:03 -0700

    Completed exercise 03 Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,14 @@
 const reverseString = function (word) {
     let string = "";
-    for (i = word.length - 1; i >= 0; i--) {
-        string += word[i]
+    for (let i = word.length - 1; i >= 0; i--) {
+        string += word[i];
     }
-    return string;
-};
-reverseString('hello there');
+    return string
+}
+reverseString('hello');
 reverseString('hello there');
 reverseString('123! abc!');
-reverseString("");
+reverseString(' ');
 
 // Do not edit below this line
 module.exports = reverseString;
